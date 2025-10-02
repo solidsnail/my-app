9171ed4f4910060b26db72d99e688a8d5f6f6712
solidsnail <rachiduoad@gmail.com>
2025-10-02 17:49:24 +0100

    update

--- library/modules/component.tsx
+++ library/modules/component.tsx
@@ -3,19 +3,12 @@ import { FC } from "hono/jsx";
 type ComponentOptionsType<P> = {
   name: string;
   render: FC<P>;
-  behaviors?: Record<string, any>;
 };
 
 export const createComponent = <P,>({
   name,
   render,
-  behaviors,
 }: ComponentOptionsType<P>) => {
-  // Attach behaviors to render function for access within component
-  if (behaviors) {
-    (render as any).behaviors = behaviors;
-  }
-
   render.displayName = name;
   return render;
 };
